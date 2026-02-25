e67a370fbd61b26339365eb612e100a802ac4d4a
Sameerali0 <sameer.ali.professional@gmail.com>
2026-02-25 16:58:06 +0500

    get videos ids

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ enterBtn.addEventListener("click", ()=>{
     fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${playlistId}&maxResults=50&key=${apiKey}`)
         .then((res) => res.json())
         .then((data)=>{
-            console.log("result ===>", data);
+            console.log("result ===>", data.items);
+            let videoIds = []
+
+            for (let i = 0; i < data.items.length; i++) {
+
+                let videoId = data.items[i].contentDetails.videoId
+                videoIds.push(videoId)
+            }
+            console.log("all video ids ===>", videoIds);
+            
             
         })
         .catch((error)=>{
