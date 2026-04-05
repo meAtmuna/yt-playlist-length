a6ddee1152dff8802e1d65bf99792da1eca0b62d
Sameerali0 <sameer.ali.professional@gmail.com>
2026-04-05 23:29:40 +0500

    copy my old css in App.css file and images in assets folder and convert my html structure to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,28 @@ function App() {
 
   return (
     <>
-      
+      <div className="main-container">
+        <header>
+            <div className="logo-icon">
+                <img src="images/youtube.png" alt="logo"/>
+            </div>
+            <div className="head-text">
+                <h1>Youtube Playlist length</h1>
+                <p>Calculate total time duration of any YouTube playlist</p>
+            </div>
+        </header>
+
+        <div className="input-section">
+            <label className="input-label" htmlFor="Playlist-url">Playlist URL</label>    
+            <div className="input-row">    
+                <input type="url" id="playlist-url" placeholder="https://www.youtube.com/playlist?list=..."/>
+                <button id="enter-btn">Get Playlist length</button>
+            </div>
+            <p id="error-msg"></p>
+        </div>
+
+        <div id="result"></div>
+      </div>
     </>
   )
 }
